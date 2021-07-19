af4f8aa4d8e5d7256621c6ba9c8cd7631a1e6dfb
Serik Klement <serikklement@gmail.com>
2021-07-19 23:55:30 +0300

    create smart contract create token

---
+++ BuyCars.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+import "./IERC20.sol";
+
+contract BuyCars {
+    address public owner;
+    address public bonusTokenAddress;
+
+    event createOrderEvent(uint256 _tokens);
+
+    constructor(address _bonusTokenAddress) {
+        owner = msg.sender;
+        bonusTokenAddress = _bonusTokenAddress;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can do this");
+        _;
+    }
+
+    uint256 public UserID;
+    uint256 public CarID;
+    uint256 public OrderID;
+
+    struct car {
+        uint256 price;
+        string brand;
+        string model;
+    }
+
+    struct order {
+        uint256 userID;
+        uint256 carID;
+        uint256 date;
+        bool active;
+        uint256 tokens;
+    }
+
+    struct serviceOrder {
+        address userAddress;
+        uint price;
+        uint date;
+    }
+
+    struct user {
+        string name;
+        uint256 tokens;
+        uint256[] orders;
+    }
+
+    mapping(address => user) public Users;
+    mapping(uint256 => address) public UserIDs;
+    mapping(uint256 => car) public Cars;
+    mapping(uint256 => order) public Orders;
+    mapping(uint => serviceOrder) public ServiceOrders;
+
+    //FUNCTIONS
+
+    function setBonusTokenAddress(address _address) public onlyOwner {
+        bonusTokenAddress = _address;
+    }
+
+    function addUser(string memory _name, address _userAddress)
+        public
+        onlyOwner
+    {
+        UserID += 1;
+        Users[_userAddress].name = _name;
+        UserIDs[UserID] = _userAddress;
+    }
+
+    function addCar(
+        string memory _brand,
+        string memory _model,
+        uint256 _price
+    ) public onlyOwner {
+        CarID += 1;
+        Cars[CarID].brand = _brand;
+        Cars[CarID].model = _model;
+        Cars[CarID].price = _price;
+    }
+
+    function createOrder(uint256 _car_id, uint256 _user_id) public onlyOwner {
+        OrderID += 1;
+        Orders[OrderID].userID = _user_id;
+        Orders[OrderID].carID = _car_id;
+        Orders[OrderID].date = block.timestamp;
+        Orders[OrderID].active = true;
+        address _user_address = UserIDs[_user_id];
+        Users[_user_address].orders.push(OrderID);
+        uint256 _tokens = Cars[_car_id].price / 100;
+        Orders[OrderID].tokens = _tokens;
+        Users[_user_address].tokens = _tokens;
+        IERC20 _bonusToken = IERC20(bonusTokenAddress);
+        _bonusToken.mint(_user_address, _tokens * 10**18);
+        emit createOrderEvent(_tokens);
+    }
+
+}
